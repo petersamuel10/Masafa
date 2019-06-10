43560c9a6e5f99e81918e3d3bf71813a3717a838
petersamuel10 <petersamuelyakoub@gmail.com>
2019-06-10 18:09:08 +0300

    design

---
+++ app/src/main/java/com/vavisa/masafah/tap_add/AddShipmentActivity.java
@@ -0,0 +1,51 @@
+package com.vavisa.masafah.add_shipment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.vavisa.masafah.R;
+import com.vavisa.masafah.activities.BaseActivity;
+import com.vavisa.masafah.companies.Companies;
+import com.vavisa.masafah.util.BottomSpaceItemDecoration;
+
+import java.util.ArrayList;
+
+public class AddShipmentActivity extends BaseActivity {
+
+  private RecyclerView shipmentList_rec;
+  private ArrayList<NewShipmentModel> shipmentList;
+  private NewShipmentAdapter adapter;
+  private Button nextButton;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_new_shipment);
+
+    shipmentList_rec = findViewById(R.id.shipment_items);
+    nextButton = findViewById(R.id.next_button);
+
+    shipmentList_rec.setLayoutManager(new LinearLayoutManager(this));
+
+    shipmentList = new ArrayList<>();
+    shipmentList.add(new NewShipmentModel("",1));
+
+    shipmentList_rec.addItemDecoration(new BottomSpaceItemDecoration(50));
+
+    adapter = new NewShipmentAdapter(shipmentList);
+    shipmentList_rec.setAdapter(adapter);
+
+    nextButton.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            startActivity(new Intent(AddShipmentActivity.this, Companies.class));
+          }
+        });
+  }
+}
---
+++ app/src/main/java/com/vavisa/masafah/tap_my_shipment/shipment_details/ShipmentDetailsFragment.java
@@ -0,0 +1,67 @@
+package com.vavisa.masafah.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.vavisa.masafah.R;
+import com.vavisa.masafah.activities.CompanyDetailsActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.vavisa.masafah.activities.MainActivity.navigationView;
+
+public class ShipmentDetailsFragment extends BaseFragment {
+
+  private View fragment;
+  private List<Integer> images = new ArrayList<>();
+  private TextView deliveryCompanyName;
+
+  @Nullable
+  @Override
+  public View onCreateView(
+      @NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+
+    if (fragment == null) {
+      fragment = inflater.inflate(R.layout.fragment_shipment_details, container, false);
+
+      deliveryCompanyName = fragment.findViewById(R.id.delivery_company_name);
+
+      RecyclerView.LayoutManager layoutManager =
+          new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+
+    } else {
+      for (int i = 1; i < navigationView.getMenu().size(); i++) {
+        navigationView.getMenu().getItem(i).setChecked(false);
+      }
+      navigationView.getMenu().getItem(0).setChecked(true);
+    }
+
+    images.add(R.drawable.sofa_single);
+    images.add(R.drawable.flooar_mat);
+    images.add(R.drawable.fridge);
+
+
+    deliveryCompanyName.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            startActivity(new Intent(getActivity(), CompanyDetailsActivity.class));
+          }
+        });
+
+    return fragment;
+  }
+
+}
